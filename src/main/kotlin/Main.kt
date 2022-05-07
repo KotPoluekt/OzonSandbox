f3329dfe8ab3bf52869cf7fac033b80666d03bf6
zokeez <birukov.alex@gmail.com>
2022-05-07 23:44:47 +0500

    Test1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 fun main(args: Array<String>) {
-    val s = readLine()
 
-    println(s)
+    val list = IntArray(Integer.parseInt(readLine()))
+
+    for (i in list.indices) {
+        val str = readLine()
+        val first = Integer.parseInt(str?.substring(0, str.indexOf(" ")))
+        val second = Integer.parseInt(str?.substring(str.indexOf(" ") + 1))
+        list[i] = first + second
+    }
+
+    for (s in list) {
+        println(s)
+    }
 }
