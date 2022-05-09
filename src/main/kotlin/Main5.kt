72aa37866e7f69479e70ad65d21820c08167ead5
zokeez <birukov.alex@gmail.com>
2022-05-09 19:02:25 +0500

    Test5 - отлично!

---
+++ src/main/kotlin/Main5.kt
@@ -0,0 +1,52 @@
+data class Task(val time: Int, val duration: Int)
+
+fun doWork(list: ArrayList<Task>) {
+
+    val result = ArrayList<Int>()
+
+    var currentTime = 0
+    for (task in list) {
+
+        if (currentTime == 0) {
+            result.add(task.time + task.duration)
+            currentTime = task.time + task.duration
+        } else if (currentTime < task.time) {
+            result.add(task.time + task.duration)
+            currentTime = task.time + task.duration
+        } else {
+            result.add(currentTime + task.duration)
+            currentTime += task.duration
+        }
+    }
+
+    val builder = StringBuilder()
+    result.map { builder.append("$it ") }
+    println(builder.toString().trim())
+}
+
+fun main(args: Array<String>) {
+
+    val setAmount = Integer.parseInt(readLine())
+
+    val setList = ArrayList<ArrayList<Task>>()
+
+    for (i in 1 .. setAmount) {
+        readLine()
+        val taskAmount = Integer.parseInt(readLine())
+
+        val arr = ArrayList<Task>()
+        for (j in 1 .. taskAmount) {
+            val str = readLine()
+            arr.add(Task(str!!.split(" ")[0].toInt(), str!!.split(" ")[1].toInt()))
+        }
+
+        if (arr != null) {
+            setList.add(arr)
+        }
+    }
+
+    // prioritizing
+    for (list in setList) {
+        doWork(list)
+    }
+}
